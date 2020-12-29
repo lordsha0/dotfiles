49ad01454858a19ee3ea4d5f59ff49bb430782cf
lordsha0 <lordsha0@protonmail.ch>
2020-12-28 19:38:20 -0500

    Setup Artix

---
+++ polybar/launch.sh
@@ -0,0 +1,8 @@
+killall -q polybar
+
+#polybar desktops -c  ~/.config/polybar/config &
+#sleep 1 &&
+#polybar time -c ~/.config/polybar/config &
+#sleep 1 &&
+#polybar info -c ~/.config/polybar/config &
+polybar master -c ~/.config/polybar/config &
