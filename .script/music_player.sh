94bff3e2a0a0ee0988422cc306e6cdaca640a23b
lordsha0 <lordsha0@protonmail.ch>
2021-01-04 08:03:35 -0500

    added Playing/Paused status

--- .script/music_player.sh
+++ .script/music_player.sh
@@ -1,3 +1,11 @@
 strTitle=$(mocp -i 2> /dev/null | grep "SongTitle")
 strArtist=$(mocp -i 2> /dev/null | grep "Artist")
-echo [${strArtist:8} - ${strTitle:11}]
+strState=$(mocp -i 2> /dev/null | grep "State")
+if [ ${strState:7} = "PLAY" ]
+then
+    echo Playing: ${strArtist:8} - ${strTitle:11}
+fi
+if [ ${strState:7} = "PAUSE" ]
+then
+    echo Paused: ${strArtist:8} - ${strTitle:11}
+fi
